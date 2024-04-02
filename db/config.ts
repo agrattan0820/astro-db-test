570e28e3bb2a743603aede1b5bad761591df9753
GameDog9988 <alex@grattan.me>
2024-04-01 20:49:30 -0400

    feat: initial commit with tables from docs

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,20 @@
-import { defineDb } from 'astro:db';
+import { column, defineDb, defineTable } from "astro:db";
+
+const Author = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    name: column.text(),
+  },
+});
+
+const Comment = defineTable({
+  columns: {
+    authorId: column.number({ references: () => Author.columns.id }),
+    content: column.text(),
+  },
+});
 
 // https://astro.build/db/config
 export default defineDb({
-	tables: {},
+  tables: { Author, Comment },
 });
--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,16 @@
-import { db } from 'astro:db';
+import { db, Author, Comment } from "astro:db";
 
 export default async function seed() {
-	// Seed local development data.
-	// See https://docs.astro.build/en/guides/astro-db/#seed-your-database
+  // Seed local development data.
+  // See https://docs.astro.build/en/guides/astro-db/#seed-your-database
+
+  await db.insert(Author).values([
+    { id: 1, name: "Alexander Grattan" },
+    { id: 2, name: "John Doe" },
+  ]);
+
+  await db.insert(Comment).values([
+    { authorId: 1, content: "Hope you like Astro DB!" },
+    { authorId: 2, content: "Enjoy!" },
+  ]);
 }
